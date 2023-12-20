5382a561460252f14edeadb77495379cc3ff66e1
alexbike2001 <73705263+alexbike2001@users.noreply.github.com>
2023-12-20 15:37:47 +0100

    Incluyo una forma ingeniosa de despedirse
    Porque lo usa muy poca gente

--- PROYECTO PRUEBA/src/Principal.java
+++ PROYECTO PRUEBA/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Holi Wankenobi");
 		System.out.println("Agur yogurt!");
+		System.out.println("Chao pescao");
 	}
 
 }
